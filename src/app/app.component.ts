23ca1e0cbbc2741befdb3044a396599a6769b990
Tommy Balestreri <tcbstl@gmail.com>
2019-10-26 14:53:58 -0500

    first 5 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];  
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          for (let i = 0; i < fetchedSatellites.length; i++){
+          let satellite = new Satellite(fetchedSatellites[i].name,fetchedSatellites[i].type,fetchedSatellites[i].launchDate,fetchedSatellites[i].orbitType,fetchedSatellites[i].operational);
+          this.sourceList.push(satellite);
+          }
+       }.bind(this));
+    }.bind(this));
+ 
+ }
 }
